2177cc249dc016b2ffd251f4fe09830c18fb4362
Nikilandgelo <niki_landgelo@outlook.com>
2024-10-17 13:26:53 +0300

    formatted main logic by git hook

--- find_messages.py
+++ find_messages.py
@@ -1,46 +1,52 @@
-from telethon.tl.types import Channel
-from telethon import TelegramClient
-from asyncio import create_task, gather
-from telethon.functions import messages
-from aiogram import Bot
 import logging
+from asyncio import create_task, gather
+
+from aiogram import Bot
+from telethon import TelegramClient
+from telethon.functions import messages
+from telethon.tl.types import Channel
 from termcolor import colored
 
 
-async def proceed_new_messages(client: TelegramClient, channels: list[Channel],
-                               keywords: list[str], bot: Bot,
-                               target_user_id: str):
+async def proceed_new_messages(
+    client: TelegramClient,
+    channels: list[Channel],
+    keywords: list[str],
+    bot: Bot,
+    target_user_id: str,
+):
     result = await client(messages.GetPeerDialogsRequest(peers=channels))
     tasks: list = []
     for index, channel in enumerate(channels):
         if result.dialogs[index].unread_count == 0:
             continue
-        logging.info(
-            colored(f'Found new messages in {channel.title}.', 'green')
-        )
+        logging.info(colored(f"Found new messages in {channel.title}.", "green"))
         kwargs: dict[str] = locals()
-        kwargs['channel'] = channel
-        kwargs['last_readed_id'] = result.dialogs[index].read_inbox_max_id
+        kwargs["channel"] = channel
+        kwargs["last_readed_id"] = result.dialogs[index].read_inbox_max_id
         task = create_task(find_messages(**kwargs))
         tasks.append(task)
     await gather(*tasks)
 
 
-async def find_messages(client: TelegramClient, channel: Channel,
-                        last_readed_id: int, keywords: list[str],
-                        bot: Bot, target_user_id: str, **kwargs):
+async def find_messages(
+    client: TelegramClient,
+    channel: Channel,
+    last_readed_id: int,
+    keywords: list[str],
+    bot: Bot,
+    target_user_id: str,
+    **kwargs,
+):
     async for message in client.iter_messages(channel):
         if last_readed_id >= message.id:
             break
         try:
-            if any(keyword.lower() in message.message.lower()
-                   for keyword in keywords):
+            if any(keyword.lower() in message.message.lower() for keyword in keywords):
                 logging.info(
-                    colored(f'New matching message in {channel.title}!',
-                            'green')
+                    colored(f"New matching message in {channel.title}!", "green")
                 )
-                message_link: str = (f"https://t.me/{channel.username}"
-                                     f"/{message.id}")
+                message_link: str = f"https://t.me/{channel.username}" f"/{message.id}"
                 text: str = (
                     f"New matching message in {channel.title}, relative to"
                     f" your keywords - {','.join(keywords)}:\n\n"
@@ -49,12 +55,13 @@ async def find_messages(client: TelegramClient, channel: Channel,
                 await bot.send_message(chat_id=target_user_id, text=text)
         except AttributeError:
             logging.warning(
-                colored((f'Skipping the message in {channel.title}'
-                         f' it has no text.'), 'yellow')
+                colored(
+                    (f"Skipping the message in {channel.title}" f" it has no text."),
+                    "yellow",
+                )
             )
         finally:
             await client.send_read_acknowledge(channel, message)
             logging.info(
-                colored(f'Marked message as read in {channel.title}.',
-                        'magenta')
+                colored(f"Marked message as read in {channel.title}.", "magenta")
             )
--- main.py
+++ main.py
@@ -1,55 +1,49 @@
+import logging
+from asyncio import run, sleep
+from os import getenv
+
+from aiogram import Bot
+from colorama import just_fix_windows_console
+from dotenv import load_dotenv
 from telethon import TelegramClient
 from telethon.tl.types import Channel
-from aiogram import Bot
-from asyncio import run, sleep
-from dotenv import load_dotenv
-from os import getenv
-from find_messages import proceed_new_messages
-import logging
-from colorama import just_fix_windows_console
 from termcolor import colored
 
+from find_messages import proceed_new_messages
 
-api_id: str | None = getenv('API_ID')
-api_hash: str | None = getenv('API_HASH')
-url_channels: list[str] = [channel.strip()
-                           for channel in getenv('CHANNELS_LINKS').split(',')]
-keywords: list[str] = [keyword.strip()
-                       for keyword in getenv('KEYWORDS').split(',')]
-bot_token: str | None = getenv('BOT_TOKEN')
-target_user_id: str | None = getenv('YOUR_TELEGRAM_ID')
+api_id: str | None = getenv("API_ID")
+api_hash: str | None = getenv("API_HASH")
+url_channels: list[str] = [
+    channel.strip() for channel in getenv("CHANNELS_LINKS").split(",")
+]
+keywords: list[str] = [keyword.strip() for keyword in getenv("KEYWORDS").split(",")]
+bot_token: str | None = getenv("BOT_TOKEN")
+target_user_id: str | None = getenv("YOUR_TELEGRAM_ID")
 
 
 async def main():
-    client = TelegramClient('searching_for_messages', api_id, api_hash)
+    client = TelegramClient("searching_for_messages", api_id, api_hash)
     bot = Bot(token=bot_token)
     async with client:
-        logging.info(
-            colored(f'Getting {len(url_channels)} channels...', 'cyan')
-        )
+        logging.info(colored(f"Getting {len(url_channels)} channels...", "cyan"))
         try:
             channels: list[Channel] = await client.get_entity(url_channels)
             while True:
-                logging.info(
-                    colored('Start searching for new messages!', 'magenta')
+                logging.info(colored("Start searching for new messages!", "magenta"))
+                await proceed_new_messages(
+                    client, channels, keywords, bot, target_user_id
                 )
-                await proceed_new_messages(client, channels, keywords, bot,
-                                           target_user_id)
-                logging.info(
-                    colored('Time to sleep...', 'cyan')
-                )
+                logging.info(colored("Time to sleep...", "cyan"))
                 await sleep(300)
         except ValueError as e:
             logging.error(
-                colored(f"Error fetching messages from {channels}:\n{e}",
-                        'red')
+                colored(f"Error fetching messages from {channels}:\n{e}", "red")
             )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     logging.basicConfig(
-        level=logging.INFO,
-        format='{asctime} - {levelname}:{message}\n',
-        style='{'
+        level=logging.INFO, format="{asctime} - {levelname}:{message}\n", style="{"
     )
     just_fix_windows_console()
     load_dotenv(override=True)
